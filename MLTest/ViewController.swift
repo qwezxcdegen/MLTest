33ead93b83d8fcab11f4c191f464b85a5f31d95e
qwezxcdegen <ifomstep@icloud.com>
2023-04-23 03:22:31 +0500

    Initial Commit

---
+++ MLTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MLTest
+//
+//  Created by Степан Фоминцев on 23.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
